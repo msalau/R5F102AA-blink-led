005106587effc4018632abdb8363f14924cbf29f
Maksim Salau <msalau@iotecha.com>
2022-05-14 00:28:38 +0200

    Toggle P20

--- src/r5f102aa_llvm.c
+++ src/r5f102aa_llvm.c
@@ -2,9 +2,15 @@
 
 int main(void)
 {
+    /* Make P20 output */
+    PM2_bit.no0 = 0;
+
     for (;;)
     {
-        // TODO: add application code here
+        /* Wait a little */
+        for (volatile unsigned char i = 100; i; i--);
+        /* Toggle P20 */
+        P2_bit.no0 ^= 1;
     }
 
     return 0;
